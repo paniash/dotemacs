72f24a16825577a20fc60c3393e738a0c871cf43
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-07-21 15:04:45 +0530

    add evil bindings

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 
 (menu-bar-mode -1)
 
-(set-face-attribute 'default nil :font "Iosevka" :height 120)
+(set-face-attribute 'default nil :font "Terminus (TTF)" :height 135)
 
 (load-theme 'wombat)
 
@@ -49,7 +49,54 @@
   :config
   (ivy-mode 1))
 
+(column-number-mode)
+(global-display-line-numbers-mode t)
 
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                 term-mode-hook
+                 eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
+;; Enabling which-key package
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
 
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(auctex auctex-latexmk evil-collection evil which-key use-package ivy-rich)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
