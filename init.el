c98e5e44a84ed391d6880a360f208eebbd9a665b
paniash <public@ashishpanigrahi.com>
2025-03-19 17:01:19 +0200

    disabled org-agenda logging with variable `org-agenda-start-with-log-mode`

--- init.el
+++ init.el
@@ -147,8 +147,6 @@
   :ensure nil
   :config
   (setq org-log-done 'time)
-  ;; Adds log entries in org agenda
-  (setq org-agenda-start-with-log-mode t)
   ;; Collapse the log entries into a "drawer"
   (setq org-log-into-drawer t)
   (setq org-todo-keywords
