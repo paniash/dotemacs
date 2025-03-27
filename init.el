464c09b3c596b5c2696a764c646adae9d9a4ec78
paniash <public@ashishpanigrahi.com>
2025-03-27 17:40:28 +0200

    placeholder for a function to add daily-logs with less overhead

--- init.el
+++ init.el
@@ -182,6 +182,31 @@
   (global-set-key (kbd "C-c a") #'org-agenda)
   (global-set-key (kbd "C-c c") #'org-capture))
 
+;; (defun open-journal-file ()
+;;   (let* ((today (format-time-string "%Y%m%d"))
+;;          (path (concat (getenv "HOME") "~/org/phd-notes/logs/" today ".org"))
+;;          (hdr-list (list (concat "#+TITLE: [" today "]")
+;;                          "#+AUTHOR: Ashish Panigrahi"))
+;;          (hdr (apply 'concat
+;;                      (mapcar (lambda (s) (concat s "\n"))
+;;                              hdr-list)))
+;;          (has-hdr (lambda ()
+;;                     (save-excursion
+;;                       (goto-char (point-min))
+;;                       (search-forward "#+TITLE" nil t)))))
+;;     (message (concat "opening " path " ..."))
+;;     (find-file path)
+;;     (unless (funcall has-hdr)
+;;       (save-excursion
+;;         (goto-char (point-min))
+;;         (insert hdr)))
+;;     (message "Enjoy your journaling!")))
+
+;; (define-key global-map (kbd "C-c 1")
+;; 	    (lambda ()
+;; 	      (interactive)
+;; 	      (open-journal-file)))
+
 ;; Automatic text wrapping in all major modes
 (setq-default auto-fill-function 'do-auto-fill)
 
