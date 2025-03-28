59bb4014b35e588764641b9171325a045697ba76
paniash <public@ashishpanigrahi.com>
2025-03-28 20:31:12 +0200

    Fixed roam directory link

--- init.el
+++ init.el
@@ -240,7 +240,7 @@
 (use-package org-roam
   :ensure t
   :custom
-  (org-roam-directory "~/roam")
+  (org-roam-directory "~/org/roam")
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n i" . org-roam-node-insert))
