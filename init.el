3bcac9f20c7f04e3c87a0ca9f5b315d2fd2957cd
paniash <public@ashishpanigrahi.com>
2025-05-08 22:03:07 +0300

    fixed rss tag syntax

--- init.el
+++ init.el
@@ -779,7 +779,7 @@ Returns the new window."
   (setq elfeed-db-directory "~/.emacs.d/elfeed/")
   (setq elfeed-enclosure-default-dir "~/downloads/")
   (setq elfeed-feeds
-	'("https://www.archlinux.org/feeds/news/" archlinux
+	'(("https://www.archlinux.org/feeds/news/" archlinux)
 	  ("https://emacsredux.com/atom.xml" emacs)
 	  "https://peterwittek.com/feeds/all.atom.xml"
 	  "https://gregorygundersen.com/feed.xml"
