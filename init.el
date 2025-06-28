0a008f2fdf34b3794b0684d4e72d5879b810cd4a
paniash <public@ashishpanigrahi.com>
2025-06-28 22:08:03 +0300

    don't use "C-l" globally

--- init.el
+++ init.el
@@ -116,6 +116,7 @@
     ("C-x C-z" . nil) ; never use it
     ("C-x C-c" . nil) ; avoid accidentally exiting Emacs
     ("C-h h" . nil) ; never show that "hello" file
+    ("C-l" . nil) ; never use it
     ("C-x <left>" . nil) ; unbind the `previous-buffer' command
     ("C-x C-p" . previous-buffer)  ; rebind `previous-buffer' command
     ("C-u" . evil-scroll-up) ; explicitly bind C-u because it sometimes misbehaves in evil mode
