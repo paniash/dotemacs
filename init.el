df4f2c0855393d0df254147fbbacdd308abd909e
Ashish Panigrahi <public@ashishpanigrahi.com>
2025-07-31 13:32:48 +0300

    added sample config for denote

--- init.el
+++ init.el
@@ -364,6 +364,27 @@
 			  '(("^ *\\([-]\\) "
 			     (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•")))))))
 
+;; Denote package by Protesilaos
+;; Experimenting with it at the moment
+(use-package denote
+  :ensure t
+  :hook (dired-mode . denote-dired-mode)
+  :bind
+  (("C-c n n" . denote)
+   ("C-c n r" . denote-rename-file)
+   ("C-c n l" . denote-link)
+   ("C-c n b" . denote-backlinks)
+   ("C-c n d" . denote-dired)
+   ("C-c n g" . denote-grep))
+  :config
+  (setq denote-directory (expand-file-name "~/org/notes/"))
+
+  ;; Automatically rename Denote buffers when opening them so that
+  ;; instead of their long file name they have, for example, a literal
+  ;; "[D]" followed by the file's title.  Read the doc string of
+  ;; `denote-rename-buffer-format' for how to modify this.
+  (denote-rename-buffer-mode 1))
+
 (use-package citar
   :no-require
   :custom
