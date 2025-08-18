b0e121b751e78d4335a878226870b5df2c5927a2
paniash <public@ashishpanigrahi.com>
2025-08-18 22:51:17 +0300

    by default only display arxiv entries in elfeed

--- init.el
+++ init.el
@@ -1011,7 +1011,7 @@ Returns the new window."
   (setq elfeed-search-print-entry-function #'pani/my-search-print-fn)
   (setq elfeed-use-curl t)
   (setq elfeed-curl-max-connections 20)
-  (setq-default elfeed-search-filter "@6-months-ago")
+  (setq-default elfeed-search-filter "@6-months-ago +arxiv")
   (setq elfeed-db-directory "~/.emacs.d/elfeed/")
   (setq elfeed-enclosure-default-dir "~/downloads/")
   (setq elfeed-search-date-format '("%Y-%m-%d" 0 :left))
