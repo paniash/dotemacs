46c66874a533d93b20edccc0c88d0cd66039c4b8
Ashish Panigrahi <public@ashishpanigrahi.com>
2025-08-18 19:35:05 +0300

    updates to elfeed search buffer

--- init.el
+++ init.el
@@ -900,6 +900,14 @@ Returns the new window."
 	 ("C-c C-c" . elfeed-update))
 
   :config
+
+  ;; Helper function: safe truncation
+  (defun truncate-string-to-width-safe (str width)
+    "Truncate STR to WIDTH characters if it exceeds it, adding an ellipsis."
+    (if (> (string-width str) width)
+	(concat (substring str 0 (max 0 (- width 1))) "…")
+      str))
+
   (defun concatenate-authors (authors-list)
     "Given AUTHORS-LIST, list of plists; return string of all authors
     concatenated."
@@ -907,54 +915,11 @@ Returns the new window."
 	(format "%s et al." (plist-get (nth 0 authors-list) :name))
       (plist-get (nth 0 authors-list) :name)))
 
-  (defun pani/my-search-print-fn (entry)
-    "Print ENTRY to the buffer."
-    (let* ((date (elfeed-search-format-date (elfeed-entry-date entry)))
-	   (title (or (elfeed-meta entry :title)
-		      (elfeed-entry-title entry) ""))
-	   (title-faces (elfeed-search--faces (elfeed-entry-tags entry)))
-	   (feed (elfeed-entry-feed entry))
-	   (feed-title
-	    (when feed
-	      (or (elfeed-meta feed :title) (elfeed-feed-title feed))))
-	   (entry-authors (concatenate-authors
-			   (elfeed-meta entry :authors)))
-	   (tags (mapcar #'symbol-name (elfeed-entry-tags entry)))
-	   (tags-str (mapconcat
-		      (lambda (s) (propertize s 'face
-					      'elfeed-search-tag-face))
-		      tags ","))
-	   (title-width (- (window-width) 10
-			   elfeed-search-trailing-width))
-	   (title-column (elfeed-format-column
-			  title (elfeed-clamp
-				 elfeed-search-title-min-width
-				 title-width
-				 elfeed-search-title-max-width)
-			  :left))
-	   (authors-width 135)
-	   (authors-column (elfeed-format-column
-			    entry-authors (elfeed-clamp
-					   elfeed-search-title-min-width
-					   authors-width
-					   80)
-			    :left)))
-      (insert (propertize date 'face 'elfeed-search-date-face) " ")
-      (insert (propertize title-column
-			  'face title-faces 'kbd-help title) " ")
-      (insert (propertize authors-column
-			  'face 'elfeed-search-date-face
-			  'kbd-help entry-authors) " ")
-      ;; (when feed-title
-      ;;   (insert (propertize entry-authors
-      ;; 'face 'elfeed-search-feed-face) " "))
-      (when entry-authors
-	(insert (propertize feed-title
-			    'face 'elfeed-search-feed-face) " "))
-      (when tags
-        (insert "(" tags-str ")"))
-      )
-  )
+  ;; Define a custom color for the authors column
+  (defface elfeed-search-author-face
+    '((t :foreground "DarkOliveGreen3"))
+    "Face for displaying authors in the elfeed search buffer.")
+
   (setq elfeed-search-print-entry-function #'pani/my-search-print-fn)
   (setq elfeed-use-curl t)
   (setq elfeed-curl-max-connections 20)
