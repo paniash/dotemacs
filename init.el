f34826eeb8d1d5b73c86ce63e48189f2fb38b15b
paniash <public@ashishpanigrahi.com>
2026-05-02 01:32:33 +0300

    add custom keyboard-quit function inside use-package emacs

--- init.el
+++ init.el
@@ -121,6 +121,38 @@
   (set-face-attribute 'fixed-pitch nil :font "Hack" :height 1.0)
   (set-face-attribute 'variable-pitch nil :font "Iosevka Term" :height 1.05)
 
+  ;; Stolen from Protesilaos' config + my own for consult-focus-lines quitting
+  (defun pani/keyboard-quit-dwim ()
+    "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- When lines are hidden with `consult-focus-lines', reveal them.
+- In every other case use the regular `keyboard-quit'."
+    (interactive)
+    (cond
+     ((region-active-p)
+      (keyboard-quit))
+     ((derived-mode-p 'completion-list-mode)
+      (delete-completion-window))
+     ((> (minibuffer-depth) 0)
+      (abort-recursive-edit))
+     ((and (boundp 'consult--focus-lines-overlays)
+	   consult--focus-lines-overlays)
+      (consult-focus-lines nil t))
+     (t
+      (keyboard-quit))))
+
+  :bind
+  ("C-g" . pani/keyboard-quit-dwim)
+
   :config
   (setq kill-do-not-save-duplicates t)
   (setq echo-keystrokes-help nil) ; Emacs 30
