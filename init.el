7c2707b7d0753fb22aaeabca8fc33364c9f30722
paniash <public@ashishpanigrahi.com>
2026-05-04 21:45:58 +0300

    fix conditional for package-review-policy
    This checks if variable exists instead of checking emacs version (only
    works for 31 and above).

--- init.el
+++ init.el
@@ -10,11 +10,12 @@
 	'(("gnu-elpa" . 3)
 	  ("nongnu" . 2)
 	  ("melpa" . 1)))
-  (if (version>= emacs-version "31")
-      (setq package-review-policy nil  ; very manual at this point (needs automation)
-	    package-review-diff-command '("git" "diff" "--no-index"
-					  "--color=never"
-					  "--diff-filter=d")))
+  ; check package-review-policy only if this variable exists (available only after emacs 31)
+  (when (boundp 'package-review-policy)
+  (setq package-review-policy nil  ; very manual at this point (needs automation)
+	package-review-diff-command '("git" "diff" "--no-index"
+				      "--color=never"
+				      "--diff-filter=d")))
   ;; Initialize all packages after package.el is loaded
   (package-initialize)
   (unless package-archive-contents
