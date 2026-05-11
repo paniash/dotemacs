0622d71fb9dc474aeea0dd6cfe4414c2801c6cee
paniash <public@ashishpanigrahi.com>
2026-05-11 19:55:37 +0300

    added helpful package for better in-built help

--- init.el
+++ init.el
@@ -1548,3 +1548,15 @@ org-agenda."
   :defer 5
   :hook ((python-mode . breadcrumb-local-mode)
 	 (python-ts-mode . breadcrumb-local-mode)))
+
+(use-package helpful
+  :ensure t
+  :config
+  ;; Note that the built-in `describe-function' includes both functions
+  ;; and macros. `helpful-function' is functions only, so we provide
+  ;; `helpful-callable' as a drop-in replacement.
+  (global-set-key (kbd "C-h f") #'helpful-callable)
+
+  (global-set-key (kbd "C-h v") #'helpful-variable)
+  (global-set-key (kbd "C-h k") #'helpful-key)
+  (global-set-key (kbd "C-h x") #'helpful-command))
