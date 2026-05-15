7f487cac67d8fc4690a2d0fe3174d3a64302a672
paniash <public@ashishpanigrahi.com>
2026-05-15 08:51:38 +0300

    explicit in mode-line-format
    Currently the default setting

--- init.el
+++ init.el
@@ -98,6 +98,17 @@
   (setq visible-bell nil
         ring-bell-function #'ignore)
 
+  ;; Modeline format
+  (setq-default mode-line-format
+		'("%e" mode-line-front-space
+		  (:propertize
+		   ("" mode-line-mule-info mode-line-client mode-line-modified
+		    mode-line-remote)
+		   display (min-width (5.0)))
+		  mode-line-frame-identification mode-line-buffer-identification "   "
+		  mode-line-position (vc-mode vc-mode) "  " mode-line-modes
+		  mode-line-misc-info mode-line-end-spaces))
+
   ;; Fonts
   (set-face-attribute 'default nil :font "Hack" :height 115)
   (set-face-attribute 'fixed-pitch nil :font "Hack" :height 1.0)
