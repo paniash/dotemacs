9cbb999a864e5e87818bb715eb04d7c8f6bf92a5
paniash <public@ashishpanigrahi.com>
2026-06-06 12:12:26 +0300

    custom notmuch saved searches for jumping between aalto and migadu accounts

--- init.el
+++ init.el
@@ -890,6 +890,14 @@ keeping the size stable across `g'/`org-agenda-redo'."
            . " %-80s  ")
           ("tags" . "(%s)")))
   (setq notmuch-show-empty-saved-searches t)
+  (setq notmuch-saved-searches
+	'((:name "inbox" :query "tag:inbox" :key "i")
+	  (:name "unread" :query "tag:unread" :key "u")
+	  (:name "aalto" :query "tag:aalto and not tag:sent" :key "aa")
+	  (:name "aalto-sent" :query "tag:aalto and tag:sent" :key "as")
+	  (:name "migadu" :query "tag:migadu and not tag:sent" :key "mm")
+	  (:name "migadu-sent" :query "tag:migadu and tag:sent" :key "ms")
+	  (:name "emacs-orgmode" :query "tag:orgmode" :key "o")))
 
   ; Tags
   (setq notmuch-archive-tags nil ; I don't archive email
