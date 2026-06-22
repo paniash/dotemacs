4ad00c13e790c3d63ae41a823447402c97abc122
Ashish Panigrahi <public@ashishpanigrahi.com>
2026-06-22 23:16:42 +0300

    added explicit colors to various elements of custom org-agenda

--- init.el
+++ init.el
@@ -656,10 +656,10 @@ keeping the size stable across `g'/`org-agenda-redo'."
   ;; Provides good color contrast for custom org-agenda
   (custom-set-faces
    '(org-agenda-date ((t (:foreground "#dc8add" :weight normal))))
-   '(org-agenda-date-weekend ((t (:weight normal))))
-   '(org-scheduled-today ((t (:foreground "#8997b3"))))
-   '(org-scheduled-previously ((t (:weight normal))))
-   '(org-deadline-today ((t (:weight normal))))
+   '(org-agenda-date-weekend ((t (:foreground "#dc8add" :weight normal))))
+   '(org-scheduled-today ((t (:foreground "#7aa2f7" :weight normal))))
+   '(org-scheduled-previously ((t (:foreground "#e0af68" :weight normal))))
+   '(org-deadline-today ((t (:foreground "#f7768e" :weight normal))))
    '(org-tag ((t (:foreground "#56b6c2" :weight light))))
    )
 
